6955542205e57fe09ef5a43d6e9a105c043462a4
Peter Ramos <dchamp16@gmail.com>
2022-10-18 13:08:19 -0600

    responsive, navigator, footer
    index.html
    - nav responsive in mobile and regular screen
    - join button changed
    - added a bubble card with images
    - main content change to inline
    - footer
    project.html
    - fixed the main content
    - navigator and footer
    event.html
    - navigator and footer
    link.html
    - navigator and footer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const registrationButton = document.getElementById("registration");
+
+registrationButton.addEventListener("click", (e) => {
+  window.location.href = "https://uvu.campusgroups.com/WDC/club_signup";
+  e.preventDefault;
+});
